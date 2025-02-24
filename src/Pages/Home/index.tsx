b3e5522dffd5bc6de3090618e5a6d1fe7411d245
Emerson452 <emerson@popoyoko.fr>
2025-02-24 17:20:46 +0100

    🎨

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-container">
+      <div className="home-header">
+        <h1>Facturation</h1>
+      </div>
+      <div className="home-main">
+        <button className="home-button">En savoir plus</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
