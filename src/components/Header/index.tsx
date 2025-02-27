e19a8e2df2fb53a44f80028fe8d504219c403859
Emerson452 <emerson@popoyoko.fr>
2025-02-27 20:40:51 +0100

    🎨 improve header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Header: React.FC = () => {
-  return <header></header>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Accueil</a>
+          </li>
+          <li>
+            <a href="#">Devis</a>
+          </li>
+          <li>
+            <a href="#">Factures</a>
+          </li>
+          <li>
+            <a href="#">Clients</a>
+          </li>
+          <li>
+            <a href="#">Paramètres</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
