05720ac2608b2fad95d7385fe8ce4a5b64c8c979
Emerson452 <emerson@popoyoko.fr>
2025-07-14 15:34:28 +0200

    🚚

--- src/Pages/InvoiceForm/index.tsx
+++ src/Pages/InvoiceForm/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
-import Input from "../Input";
+import Input from "../../components/Input";
 
 interface SelectFieldProps {
   label?: string;
